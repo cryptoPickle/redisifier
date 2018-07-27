0749438003019a91953995dfe1c3dde9f874178f
Halil <o-wned@outlook.com>
2018-07-27 21:27:15 +0100

    some tests

--- src/api/__tests__/api.test.js
+++ src/api/__tests__/api.test.js
@@ -7,6 +7,17 @@ let options = {
 };
 const client = SimpleRedis();
 const client2 = SimpleRedis(options);
+
+beforeEach(() => {
+  client2.deleteKey('myKey3');
+  client2.deleteKey('myKey21');
+  client2.deleteKey('myKey3222');
+  client2.deleteKey('myKeytest');
+  client2.deleteKey('myKey123123')
+  client2.deleteKey('myKey33212')
+
+})
+
 describe("Connection", () => {
   test("Client tries to connect 127.0.0.1:6379 for default", () => {
     expect(client._client.address).toBe("127.0.0.1:6379");
@@ -18,20 +29,17 @@ describe("Connection", () => {
   });
 });
 
-afterEach(() => {
-  client2.deleteKey('myKey')
-});
 
 
 describe("Data Flow", () => {
   test("Set Key/Value", () => {
     client2.setKeyValue("myKey1", "test", (err, reply) => {
-      expect(reply).toBe("OK");
+      return expect(reply).toBe("OK");
     });
   });
   test("Get Key/Value", () => {
     client2.getKeyValue("myKey1", (err, reply) => {
-      expect(reply).toBe("test");
+      return expect(reply).toBe("test");
     });
   });
   test("Set Hash", () => {
@@ -39,45 +47,51 @@ describe("Data Flow", () => {
       "myKey3",
       { value1: "value2", value2: "value2" },
       (err, reply) => {
-        expect(reply).toBe("OK");
+        return expect(reply).toBe("OK");
       }
     );
   });
   test("Get Hash", () => {
-    client2.getHash("myKey3", (err, reply) => {
-      expect(reply).toEqual({ value1: "value2", value2: "value2" });
-    });
+    client.setHash('test12312354',{ value1: "value2", value2: "value2" }, () => {
+      client2.getHash("test12312354", (err, reply) => {
+        console.log(reply)
+        return expect(reply).toEqual({ value1: "value2", value2: "value2" });
+      });
+    })
   });
   test("Set List", () => {
-    client2.setList("myKey", ["test", "test2", "test3"], (err, reply) => {
+    client2.deleteKey('myKey21')
+    client2.setList("myKey21", ["test", "test2", "test3"], (err, reply) => {
       expect(reply).toBe(3);
     });
   });
   test("Get Lists", () => {
-    client2.setList("myKey", ["test", "test2", "test3"])
-    client.getList('myKey', (err,reply) => {
-      expect(reply).toEqual(["test", "test2", "test3"]);
+    client2.deleteKey('myKey3222', (err,resp) => {
+      client2.setList("myKey3222", ["simple", "redis"], () => {
+        client.getList('myKey3222', (err,reply) => {
+          return expect(reply).toEqual(["simple", "redis"]);
+        })
+      })
     })
   });
   test("Create Set", () => {
     client2.createSet(
-      "myKey",
+      "myKeytest",
       ["test1", "test2", "test3"],
        (err, reply) => {
-        expect(reply).toBe(3);
+        return expect(reply).toBe(3);
       }
     );
   });
   test("Get Sets", () => {
     client2.createSet(
-      "myKey",
+      "myKey123123",
       ["test1", "test2", "test3"],
       () => {
-        client2.getSet('myKey', (err,reply) => {
-          expect(reply).toEqual(["test1", "test2", "test3"])
+        client2.getSet('myKey123123', (err,reply) => {
+          return expect(reply).toEqual(["test3", "test2", "test1"])
         })
       }
     );
   });
-
 });
